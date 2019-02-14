45156e2d01673a503c0efb21852372ac451fbfd0
Ardalan Mehrani <ardalan.mehrani@besedo.com>
2019-02-14 11:39:51 +0100

    update example

--- examples/w2v_from_text.sh
+++ examples/w2v_from_text.sh
@@ -21,23 +21,23 @@ python3 -m src.train --file "data/SMSSpamCollection.txt" \
                      --null_word 0
 
 # Train from csv
-python3 -m src.train.py --file "data/movie_reviews.csv" \
-                        --input_type "csv" \
-                        --separator "," \
-                        --folder "model_from_csv_custom_params" \
-                        --columns_to_select "Phrase" \
-                        --size 10 \
-                        --alpha 0.025 \
-                        --window 5 \
-                        --min_count 5 \
-                        --max_vocab_size 100000 \
-                        --sample 1e-3 \
-                        --seed 1 \
-                        --workers 4 \
-                        --min_alpha 0.0001 \
-                        --sg 0 \
-                        --hs 0 \
-                        --negative 10 \
-                        --cbow_mean 1 \
-                        --iter 5 \
-                        --null_word 0
+python3 -m src.train --file "data/movie_reviews.csv" \
+                     --input_type "csv" \
+                     --separator "," \
+                     --folder "models/movie_reviews" \
+                     --columns_to_select "Phrase" \
+                     --size 10 \
+                     --alpha 0.025 \
+                     --window 5 \
+                     --min_count 5 \
+                     --max_vocab_size 100000 \
+                     --sample 1e-3 \
+                     --seed 1 \
+                     --workers 4 \
+                     --min_alpha 0.0001 \
+                     --sg 0 \
+                     --hs 0 \
+                     --negative 10 \
+                     --cbow_mean 1 \
+                     --iter 5 \
+                     --null_word 0
